1a96df513df9267033d05d69c64f1c7d91b395ed
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-06-30 16:26:09 +0200

    Pattern Builder for Loan

---
+++ ESGI.DesignPattern.Projet/LoanBuilder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ESGI.DesignPattern.Projet
+{
+    public class LoanBuilder
+    {
+        private double commitment = 0;
+        private DateTime start = DateTime.Now;
+        private DateTime? expiry;
+        private DateTime? maturity;
+        private CapitalStrategy strategy;
+        private int riskRating;
+        private double unusedPercentage = 1;
+
+        public Loan Build()
+        {
+            return new Loan(
+                commitment,
+                start,
+                expiry,
+                maturity,
+                riskRating,
+                strategy,
+                unusedPercentage
+            );
+        }
+
+        public LoanBuilder WithCommitment(double commitment)
+        {
+            this.commitment = commitment;
+            return this;
+        }
+
+        public LoanBuilder WithUnusedPercentage(double unusedPercentage)
+        {
+            this.unusedPercentage = unusedPercentage;
+            return this;
+        }
+
+        public LoanBuilder WithStartDate(DateTime start)
+        {
+            this.start = start;
+            return this;
+        }
+
+        public LoanBuilder WithExpiryDate(DateTime? expiry)
+        {
+            this.expiry = expiry;
+            return this;
+        }
+
+        public LoanBuilder WithMaturityDate(DateTime? maturity)
+        {
+            this.maturity = maturity;
+            return this;
+        }
+
+        public LoanBuilder WithRiskRating(int riskRating)
+        {
+            this.riskRating = riskRating;
+            return this;
+        }
+
+        public LoanBuilder WithStrategy(CapitalStrategy strategy)
+        {
+            this.strategy = strategy;
+            return this;
+        }
+
+    }
+}
